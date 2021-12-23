dad178339873052b1f8c370d83e45ebe001a230f
knmarcin <knapmarcin@icloud.com>
2021-12-23 21:08:37 +0100

    Update

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -3,9 +3,32 @@ import useToken from "../../auth/useToken";
 
 const Dashboard = () => {
   const [data, setData] = useState([]);
+  const [effect, setEffect] = useState(1);
   const { token } = useToken();
+  const [postData, setPostData] = useState({
+    habit: 1,
+    accomplished: "2023-12-22",
+  });
+
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/habits/", {
+    fetch(`http://127.0.0.1:8000/count/${postData.habit}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify({
+        ...postData,
+      }),
+    }).then((response) => {
+      console.log(postData.accomplished);
+      console.log(response.statusText);
+      if (response.ok) setEffect((effect) => effect + 1);
+    });
+  }, [postData, token]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/habits", {
       method: "get",
       headers: {
         Accept: "application/json",
@@ -21,9 +44,8 @@ const Dashboard = () => {
       })
       .then((res) => {
         setData(res);
-        console.log(res);
       });
-  }, [token]);
+  }, [token, effect]);
 
   return (
     <>
@@ -32,7 +54,15 @@ const Dashboard = () => {
         return (
           <div key={item.id}>
             <h1>
-              {item.name} {item.created_at} {item.count} {item.owner}
+              {item.name} {item.created_at}{" "}
+              <button
+                value={item.id}
+                onClick={(e) => {
+                  setPostData({ ...postData, habit: item.id });
+                }}
+              >
+                {item.count}
+              </button>
             </h1>
           </div>
         );
