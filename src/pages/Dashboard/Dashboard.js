ba6f0e8acf31453048cb87fcd32b706da502b3d7
knmarcin <knapmarcin@icloud.com>
2021-12-28 13:52:39 +0100

    Update

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -3,16 +3,22 @@ import useToken from "../../auth/useToken";
 
 const Dashboard = () => {
   const [data, setData] = useState([]);
+  const [days, setDays] = useState([]);
   const [effect, setEffect] = useState(1);
+
   const { token } = useToken();
 
   const [postData, setPostData] = useState({
-    habit: 1,
+    habit: -1,
     accomplished: "2023-12-22",
   });
 
+  const elements = [0, 1, 2, 3, 4, 5, 6];
+  const date = new Date();
+
   useEffect(() => {
-    fetch(`http://127.0.0.1:8000/count/${postData.habit}`, {
+    if (postData.habit === -1) return;
+    fetch(`http://127.0.0.1:8000/count/${postData.habit}/`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -27,7 +33,7 @@ const Dashboard = () => {
   }, [postData, token]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/habits", {
+    fetch("http://127.0.0.1:8000/habits/", {
       method: "get",
       headers: {
         Accept: "application/json",
@@ -46,22 +52,71 @@ const Dashboard = () => {
       });
   }, [token, effect]);
 
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/count/", {
+      method: "get",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    })
+      .then((response) => {
+        if (response.status === 401) {
+          document.location.href = "/logout";
+        }
+        return response.json();
+      })
+      .then((res) => {
+        setDays(res);
+      });
+  }, [token, effect]);
+
   return (
     <>
-      <h1>hello</h1>
+      <h1>Your Last 7 Days </h1>
+
       {data.map((item) => {
         return (
           <div key={item.id}>
             <h1>
-              {item.name}
-              <button
-                value={item.id}
-                onClick={(e) => {
-                  setPostData({ ...postData, habit: item.id });
-                }}
-              >
-                {item.count}
-              </button>
+              {item.name} {item.id}
+              {elements.map((index) => {
+                var nextDay = new Date(date - index * 86400000);
+                var filtered_days = days
+                  .filter(
+                    (x) =>
+                      x.accomplished ===
+                      `${nextDay.getFullYear()}-${
+                        nextDay.getMonth() + 1
+                      }-${nextDay.getDate()}`
+                  )
+                  .filter((x) => x.habit === item.id).length;
+
+                return (
+                  <div>
+                    {`${nextDay.getFullYear()}-${
+                      nextDay.getMonth() + 1
+                    }-${nextDay.getDate()}`}
+                    <button
+                      value={item.id}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setPostData({
+                          ...postData,
+                          habit: item.id,
+                          accomplished: `${nextDay.getFullYear()}-${
+                            nextDay.getMonth() + 1
+                          }-${nextDay.getDate()}`,
+                        });
+                      }}
+                    >
+                      {filtered_days === 0 ? <>Complete</> : <>Uncomplete</>}{" "}
+                    </button>
+                  </div>
+                );
+              })}
+              Total: {item.count}
             </h1>
           </div>
         );
